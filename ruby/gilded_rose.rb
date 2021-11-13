e330f020a954ef2e10410df7c0790fb72b37a82a
Aiandrox <aiandrox7@gmail.com>
2021-11-13 13:39:28 +0900

    繰り返し処理をメソッドに切り出し

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -8,26 +8,32 @@ class GildedRose
   # 1日経過時に実行する
   def update_quality
     @items.each do |item|
-      if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
-        item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+      update_each_quality(item)
+    end
+  end
+
+  private
+
+  def update_each_quality(item)
+    if (item.name != 'Aged Brie') && (item.name != 'Backstage passes to a TAFKAL80ETC concert')
+      item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+    elsif item.quality < 50
+      item.quality = item.quality + 1
+      if item.name == 'Backstage passes to a TAFKAL80ETC concert'
+        item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
+        item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
+      end
+    end
+    item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
+    if item.sell_in.negative?
+      if item.name != 'Aged Brie'
+        if item.name != 'Backstage passes to a TAFKAL80ETC concert'
+          item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
+        else
+          item.quality = item.quality - item.quality
+        end
       elsif item.quality < 50
         item.quality = item.quality + 1
-        if item.name == 'Backstage passes to a TAFKAL80ETC concert'
-          item.quality = item.quality + 1 if item.sell_in < 11 && (item.quality < 50)
-          item.quality = item.quality + 1 if item.sell_in < 6 && (item.quality < 50)
-        end
-      end
-      item.sell_in = item.sell_in - 1 if item.name != 'Sulfuras, Hand of Ragnaros'
-      if item.sell_in.negative?
-        if item.name != 'Aged Brie'
-          if item.name != 'Backstage passes to a TAFKAL80ETC concert'
-            item.quality = item.quality - 1 if item.quality.positive? && (item.name != 'Sulfuras, Hand of Ragnaros')
-          else
-            item.quality = item.quality - item.quality
-          end
-        elsif item.quality < 50
-          item.quality = item.quality + 1
-        end
       end
     end
   end
